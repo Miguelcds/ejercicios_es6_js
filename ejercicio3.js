64ae6be505af4adccf45e936cd72f720b991a699
Miguelcds <miguel-cds@hotmail.com>
2024-09-17 23:13:44 +0200

    3.3

--- ejercicio3.js
+++ ejercicio3.js
@@ -32,4 +32,23 @@ const usersFilter = users.map((user) => {
 
 })
 
-console.log(usersFilter);
+// console.log(usersFilter);
+
+// 3.3 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
+
+
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+
+const citiesFilter = cities.map((citie) => {
+	if (citie.isVisited === true) {
+	 return citie.name + ": Visited";
+	}
+	return citie.name + ": No Visited"
+})
+
+console.log(citiesFilter);
