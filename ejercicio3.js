0f7b8388128e8f12c9ce883ec4bdec3b9301329e
Miguelcds <miguel-cds@hotmail.com>
2024-09-17 18:32:47 +0200

    3.2

---
+++ ejercicio3.js
@@ -0,0 +1,35 @@
+// 3.1 Map()
+
+// const users = [
+// 	{id: 1, name: 'Abel'},
+// 	{id:2, name: 'Julia'},
+// 	{id:3, name: 'Pedro'},
+// 	{id:4, name: 'Amanda'}
+// ];
+
+// const usersName = users.map(item => item.name)
+
+// console.log(usersName);
+
+
+// 3.1 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
+
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+
+const usersFilter = users.map((user) => {
+	if (user.name.startsWith("A")) {
+		user.name = "Anacleto"
+		return user.name
+	}
+
+	return user.name
+
+})
+
+console.log(usersFilter);
